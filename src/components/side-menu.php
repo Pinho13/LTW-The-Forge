151eff0079568f5278a16b1383c5cf374ad7cb54
T0m2sT <pedrotomasteixeira@gmail.com>
2026-04-23 08:03:18 +0100

    refactor: side-menu function

---
+++ src/components/side-menu.php
@@ -0,0 +1,12 @@
+<nav class="side-menu">
+    <?php include __DIR__ . '/logo.php'; ?>
+
+    <div class="side-nav">
+        <a href="/src/pages/my-account.php"      <?= $activePage === 'account'      ? 'class="active"' : '' ?>>MY ACCOUNT</a>
+        <a href="/src/pages/classes.php"         <?= $activePage === 'classes'      ? 'class="active"' : '' ?>>CLASSES</a>
+        <a href="/src/pages/trainers.php"        <?= $activePage === 'trainers'     ? 'class="active"' : '' ?>>TRAINERS</a>
+        <a href="/src/pages/my-classes.php"      <?= $activePage === 'my-classes'   ? 'class="active"' : '' ?>>MY CLASSES</a>
+        <a href="/src/pages/reservations.php"    <?= $activePage === 'reservations' ? 'class="active"' : '' ?>>RESERVATIONS</a>
+        <a href="/src/pages/news.php"            <?= $activePage === 'news'         ? 'class="active"' : '' ?>>NEWS</a>
+    </div>
+</nav>
