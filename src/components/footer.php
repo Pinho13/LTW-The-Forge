ffa50ec45612fbb02c05130b446f42dc8b429a77
T0m2sT <pedrotomasteixeira@gmail.com>
2026-04-23 08:07:55 +0100

    components

---
+++ src/components/footer.php
@@ -0,0 +1,17 @@
+<footer>
+    <span class="footer-text">THE FORGE</span>
+
+    <div class="symbol-container">
+        <span class="symbol layer-1">&times;</span>
+        <span class="symbol layer-2">&times;</span>
+    </div>
+
+    <span class="footer-text">TRAINING FACILITIES</span>
+
+    <div class="symbol-container">
+        <span class="symbol layer-1">&times;</span>
+        <span class="symbol layer-2">&times;</span>
+    </div>
+
+    <span class="footer-text">2026</span>
+</footer>
---
+++ src/components/logo.php
@@ -0,0 +1,4 @@
+<label for="mobile-menu-toggle" class="logo">
+    <img src="../assets/images/logo-no-bg.png" alt="The Forge Logo" class="logo-img">
+    <span class="logo-text">THE FORGE</span>
+</label>
