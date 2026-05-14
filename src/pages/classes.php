ead64daeb2d42de392267530538e75cd5fab96c6
Miguel Rocha <miguelmirandarocha@gmail.com>
2026-05-14 20:50:20 +0100

    Standardized error message color

--- src/pages/classes.php
+++ src/pages/classes.php
@@ -1,9 +1,8 @@
 <?php
 declare(strict_types=1);
-require_once(__DIR__ . '/../../utils/session.php');
+require_once(__DIR__ . '/../../utils/page_bootstrap.php');
 
-$session = new Session();
-$session->requireLogin('/src/pages/index.php?open=login');
+[$session] = requireAuthenticatedPage();
 ?>
 <!DOCTYPE html>
 <html lang="en-US">
@@ -17,17 +16,6 @@ $session->requireLogin('/src/pages/index.php?open=login');
 </head>
 
 <body>
-    <input type="checkbox" id="side-menu-toggle">
-
-    <header class="mobile-top-bar">
-        <label for="side-menu-toggle" class="logo">
-            <img src="../assets/images/logo-no-bg.png" alt="The Forge Logo" class="logo__img">
-            <span class="logo__text">THE FORGE</span>
-        </label>
-    </header>
-
-    <label for="side-menu-toggle" class="side-menu-backdrop"></label>
-
     <?php $activePage = 'classes'; include '../components/side-menu.php'; ?>
 
     <main>
