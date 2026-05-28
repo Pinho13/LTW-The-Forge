e9af834a90c697899699b8100f71777b994508eb
Pinho <rafaelpinhotsilva@gmail.com>
2026-05-28 17:23:07 +0100

    Small fix after utils refactor

--- src/utils/page_bootstrap.php
+++ src/utils/page_bootstrap.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 date_default_timezone_set('Europe/Lisbon');
 require_once(__DIR__ . '/deny_direct_access.php');
 require_once(__DIR__ . '/session.php');
-require_once(__DIR__ . '/../database/connection.db.php');
+require_once(__DIR__ . '/../../database/connection.db.php');
 
 
 function requireAuthenticatedPage(string $redirect = '/src/pages/index.php?open=login'): array
