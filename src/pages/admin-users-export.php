a36ba54e586d37aadc603a933f319c7b69c7ad72
Pinho <rafaelpinhotsilva@gmail.com>
2026-05-30 19:06:51 +0100

    Fixed utils path in all files

--- src/pages/admin-users-export.php
+++ src/pages/admin-users-export.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-require_once(__DIR__ . '/../../utils/page_bootstrap.php');
+require_once(__DIR__ . '/../utils/page_bootstrap.php');
 require_once(__DIR__ . '/../../database/models/AdminUser.class.php');
 
 [$session, $db] = requireAuthenticatedPage();
--- src/pages/index.php
+++ src/pages/index.php
@@ -3,7 +3,7 @@
   require_once(__DIR__ . '/../utils/session.php');
   require_once(__DIR__ . '/../templates/common.tpl.php');
   require_once(__DIR__ . '/../../database/models/ClassCatalog.class.php');
-  require_once(__DIR__ . '/../../utils/page_bootstrap.php');
+  require_once(__DIR__ . '/../utils/page_bootstrap.php');
 
   $session = new Session();
   $db = getDatabaseConnection();
